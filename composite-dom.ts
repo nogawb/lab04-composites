4137ef668c012fc14479faf419cea753e239d5f5
Benjamin Nogawa <benjamin.nogawa@gmail.com>
2017-01-24 20:55:36 -0800

    this is my lab stuff

--- composite-dom.ts
+++ composite-dom.ts
@@ -2,3 +2,23 @@ interface IDomElement {
   print();
 }
 
+class TextNode {
+  console.print('</p>');
+
+}
+
+class DomElement {
+  private tag:string;
+
+  constructor(tag:string){
+    this.tag = tag;
+  }
+
+  isTextNode(){
+    if (this.tag == '<p>') {
+      console.log(<p>)
+      this.TextNode();
+    }
+  }
+}
+
